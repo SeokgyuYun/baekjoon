8b51ae38881cd013e7f4d23945d11938943fab6e
Seokgyu <seokgyu1124@naver.com>
2022-12-22 09:12:39 +0900

    ✨ Add: 1271 -  엄청난 부자2

---
+++ Kotlin/src/main/kotlin/23560.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    var cnt = 0
+
+}
+// 1 2 3 4 5 6
+// 1 2 3 6 5 4
+// 3 2 1 4 5 6
+// 3 2 1 6 5 4
