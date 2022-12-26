5ef420cf65b36b49328de317b614588e866432c5
Seokgyu <seokgyu1124@naver.com>
2022-12-26 19:56:25 +0900

    ✨ 1934 - 최소공배수

--- Kotlin/src/main/kotlin/1934.kt
+++ Kotlin/src/main/kotlin/1934.kt
@@ -2,11 +2,11 @@ import java.util.Scanner
 
 fun main() {
     val sc = Scanner(System.`in`)
-    val n = sc.nextInt()
-    for (i in 0 until n) {
+    for (i in 0 until sc.nextInt()) {
         val a = sc.nextInt()
         val b = sc.nextInt()
-        println("${a * b / gcd(a, b)} ${gcd(a, b)}")
+        println(a * b / gcd(a, b))
     }
 }
+
 private fun gcd(a: Int, b:Int): Int = if(b != 0) gcd(b, a % b) else a
