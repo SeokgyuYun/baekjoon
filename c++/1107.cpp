67a20f8c99cfce1098d9a573d4bf576751fe5082
윤석규 <devSxxkgyu@gmail.com>
2023-03-24 23:26:34 +0900

    Rename Directory C to c++

---
+++ c++/1107.cpp
@@ -0,0 +1,14 @@
+// 1107번: 리모컨
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, M, channel = 100, arr[10];
+    cin >> N >> M;
+    for (int i = 0; i < M; i++)
+        cin >> arr[i];
+    
+
+    return 0;
+}
