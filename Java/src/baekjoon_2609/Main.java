42a57a3ec66d9fa0fbc122e7334b64c1f081d3a2
윤석규 <greeentea.dev@gmail.com>
2023-04-15 18:00:10 +0900

    [Java] 2609번: 최대공약수와 최소공배수

---
+++ Java/src/baekjoon_2609/Main.java
@@ -0,0 +1,22 @@
+package baekjoon_2609;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcd(a, b));
+        System.out.println(lcm(a, b));
+        sc.close();
+    }
+
+    static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
